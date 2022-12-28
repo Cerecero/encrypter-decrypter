e4cc45bffa1eb238f346e9d1fe8b5e32f8497c62
Cerecero <ale.rey.cer@outlook.com>
2022-12-28 14:12:33 -0600

    added a button that later on will copy to the clipboard

--- script/script.js
+++ script/script.js
@@ -1,19 +1,52 @@
 const buttonEncrypt = document.getElementById("encrypt");
 const buttonDecrypt = document.getElementById("decrypt");
 
+const buttonCopy = () => {
+    const divResultado = document.getElementById("resultadosDiv");
+    const buttonCopy = document.createElement("button");
+
+    buttonCopy.style.backgroundColor = '#FFF';
+    buttonCopy.style.color = '#0A3871';
+    buttonCopy.style.border = '2px solid #0A3871';
+    buttonCopy.textContent = 'Copiar';
+    buttonCopy.style.padding = "24px";
+    buttonCopy.style.gap = "8px";
+    buttonCopy.style.width =  "328px";
+    buttonCopy.style.height = "67px";
+    buttonCopy.style.borderRadius= "24px";
+    buttonCopy.style.fontWeight= "400";
+    buttonCopy.style.fontSize="16px";
+    buttonCopy.style.lineHeight= "19px";
+    buttonCopy.style.textAlign= "center";
+    
+    divResultado.appendChild(buttonCopy);
+};
+
+
+const hideUI = () => { /*A simple function that hides the elements*/
+    
+    buttonCopy();
+    const imgMunheco = document.getElementById("munheco");
+    const placeholderText = document.getElementById("placeholderText");
+    imgMunheco.style.display = "none" ;
+    placeholderText.style.display = "none" ;  
+}
+
 buttonEncrypt.addEventListener("click", () => {
     let text = document.getElementById("text").value;
     let resultado = document.getElementById("result");
+    hideUI();
+    return resultado.innerHTML = text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     /*if (text.match(/[A-Z]/)){
         alert("Solo se permiten letras minúsculas");
     }*/
     /*warning.style.color = "red";*/
-    return resultado.innerHTML = text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    
 });
 
 buttonDecrypt.addEventListener("click", () => {
     let text = document.getElementById("text").value;
     let resultado = document.getElementById("result");
-
+    hideUI();
     return resultado.innerHTML = text.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g,"u");
 });
