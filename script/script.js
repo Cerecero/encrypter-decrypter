8b17f0ce2373d024d708d9f61df4d793f334a293
Cerecero <ale.rey.cer@outlook.com>
2022-12-28 13:21:15 -0600

    added a decyper function

--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
 const buttonEncrypt = document.getElementById("encrypt");
+const buttonDecrypt = document.getElementById("decrypt");
+
 buttonEncrypt.addEventListener("click", () => {
     let text = document.getElementById("text").value;
     let resultado = document.getElementById("result");
-    
-if (text.match(/[A-Z]/)){
+    /*if (text.match(/[A-Z]/)){
         alert("Solo se permiten letras minúsculas");
-    }
+    }*/
     /*warning.style.color = "red";*/
     return resultado.innerHTML = text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+});
+
+buttonDecrypt.addEventListener("click", () => {
+    let text = document.getElementById("text").value;
+    let resultado = document.getElementById("result");
+
+    return resultado.innerHTML = text.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g,"u");
 });
